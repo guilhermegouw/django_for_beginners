0fa8f11cf3f884a03365d1f6f72dd79640feabe2
guilhermegouw <guilherme.gouw@gmail.com>
2022-03-01 11:59:45 -0300

    Chapter 5

---
+++ chapter5/blog/tests.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_user", email="user@testmail.com", password="secret"
+        )
+
+        self.post = Post.objects.create(
+            title="Interesting post",
+            body="Some very informational body",
+            author=self.user,
+        )
+
+    def test_post_detail_view(self):
+        response = self.client.get("http://127.0.0.1:8000/post/1")
+        no_response = self.client.get("http://127.0.0.1:8000/post/1000")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Interesting post")
+        self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_string_representation(self):
+        post = Post(title="Some title")
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(self.post.title, "Interesting post")
+        self.assertEqual(self.post.body, "Some very informational body")
+        self.assertEqual(self.post.author, self.user)
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Interesting post")
+        self.assertTemplateUsed(response, "home.html")
